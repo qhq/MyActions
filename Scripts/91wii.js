27029300711bff4ed8f09b49a4c3f88a336729b5
qhq <qhqcz@qq.com>
2020-10-09 23:27:56 +0800

    Update 91wii.js

--- Scripts/91wii.js
+++ Scripts/91wii.js
@@ -42,6 +42,7 @@ function checkin() {
       //"Host": "rdcseason.m.jd.com",
       //"Accept": "application/json, text/plain, */*",
       "Connection": "keep-alive",
+      "Content-Type": "application/x-www-form-urlencoded; charset=gb2312",
       "Cookie": CookieMe || $.getdata("CookieMe"),
       "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.1 Mobile/15E148 Safari/604.1",
       //"Accept-Language": "zh-cn",
