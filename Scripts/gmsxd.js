fb5a46059558338dd1d37ce15f7e5681069a4560
qhq <qhqcz@qq.com>
2020-10-22 16:19:57 +0800

    Update gmsxd.js

--- Scripts/gmsxd.js
+++ Scripts/gmsxd.js
@@ -13,7 +13,7 @@ const KEY = process.env.COOKIE_GMSXD
 const SEND_KEY = process.env.SEND_KEY
 
 async function downFile () {
-    const url = 'https://raw.githubusercontent.com/qhq/Auto_sign/master/Scripts/sxd.js?token=AAZKMWOUANAIRKIQFLKCOHS7SE7WI'
+    const url = 'https://raw.githubusercontent.com/qhq/Auto_sign/master/Scripts/sxd.js'
     await download(url, './')
 }
 
