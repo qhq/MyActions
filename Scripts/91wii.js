997f368388cfa5c9378e7d080605b77155c05063
qhq <qhqcz@qq.com>
2020-10-23 15:57:12 +0800

    Update 91wii.js

--- Scripts/91wii.js
+++ Scripts/91wii.js
@@ -10,7 +10,7 @@ const notify = $.isNode() ? require('../sendNotify') : ''
 const CookieQ = process.env.COOKIE_91WII
 const formhash = process.env.COOKIE_91WII_FORMHASH
 
-var date = new Date();
+//var date = new Date();
 checkin()
 
 function checkin() {
@@ -41,7 +41,7 @@ function checkin() {
                     if (data.match(/(ÒÑÍê³É|\u606d\u559c\u60a8|ǩ���ɹ�~�����������ֵ)/)) {
                         $.msg(`${$.name}`, "", date.getMonth() + 1 + "月" + date.getDate() + "日，签到成功 🎉")
                     } else if (data.match(/(ÄúÒÑ|\u5DF2\u7ECF\u7B7E\u8FC7\u5230|�������Ѿ�ǩ����)/)) {
-                        //$.msg(`${$.name}`, "", date.getMonth() + 1 + "月" + date.getDate() + "日，已签过 ⚠️")
+                        $.msg(`${$.name}`, "", date.getMonth() + 1 + "月" + date.getDate() + "日，已签过 ⚠️")
                     } else if (data.match(/(ÏÈµÇÂ¼|\u9700\u8981\u5148\u767b\u5f55|\u767b\u5f55|�Ҫ�ȵ�¼���ܼ�|系统拒绝|���ϵͳ�ܾ�)/)) {
                         $.msg(`${$.name}`, "", "签到失败，Cookie 失效 ‼️‼️");
                         //if ($.isNode()) {
