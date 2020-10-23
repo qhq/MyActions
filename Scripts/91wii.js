059163ac8d10aca4f479048732259ed1a30c4706
qhq <qhqcz@qq.com>
2020-10-23 15:01:19 +0800

    Update and rename 91wii2.js to 91wii.js

--- Scripts/91wii.js
+++ Scripts/91wii.js
@@ -20,8 +20,10 @@ async function downFile () {
 
 async function changeFiele () {
     let content = await fs.readFileSync('./temp.js', 'utf8')
-    content = content.replace(/process\.env\.COOKIE_91WII\.split\(('&'|'\\n'|)\);/, `'${KEY}';`)
-    content = content.replace(/process\.env\.COOKIE_91WII_FORMHASH\.split\(('&'|'\\n'|)\);/, `'${FH}';`)
+    //content = content.replace(/process\.env\.COOKIE_91WII\.split\(('&'|'\\n'|)\);/, `'${KEY}';`)
+    //content = content.replace(/process\.env\.COOKIE_91WII_FORMHASH\.split\(('&'|'\\n'|)\);/, `'${FH}';`)
+    content = content.replace(/process\.env\.COOKIE_91WII/, `'${KEY}'`)
+    content = content.replace(/process\.env\.COOKIE_91WII_FORMHASH/, `'${FH}'`)
     await fs.writeFileSync( './execute.js', content, 'utf8')
     console.log(content);
 }
