c75baceab2a71b0053603d19c35c51c02ad1baf9
qhq <qhqcz@qq.com>
2020-10-26 22:44:05 +0800

    Update smartReplace.js

--- smartReplace.js
+++ smartReplace.js
@@ -4,7 +4,7 @@ const download = require("download");
 async function replaceWithSecrets(content, Secrets) {
     if (!Secrets || !Secrets) return content;
     const replacements = [];
-    //await init_notify(Secrets, content, replacements);
+    await init_notify(Secrets, content);
         if (Secrets.DETECT_URL) {
             //replacements.push({ key: /url = []/, value: "url = " + JSON.stringify(Secrets.DETECT_URL.split("\n")) });
             replacements.push({ key: /url = \[\]/, value: `qhq` });
@@ -16,13 +16,12 @@ async function replaceWithSecrets(content, Secrets) {
 }
 function batchReplace(content, replacements) {
     for (var i = 0; i < replacements.length; i++) {
-        console.log(replacements[i].key)
         content = content.replace(replacements[i].key, replacements[i].value);
     }
     return content;
 }
-/*
-async function init_notify(Secrets, content, replacements) {
+
+async function init_notify(Secrets, content) {
     if (!Secrets.PUSH_KEY && !Secrets.BARK_PUSH && !Secrets.TG_BOT_TOKEN) {
         if (content.indexOf("require('./sendNotify')") > 0) {
             replacements.push({
@@ -31,32 +30,12 @@ async function init_notify(Secrets, content, replacements) {
                     "{sendNotify:function(){},serverNotify:function(){},BarkNotify:function(){},tgBotNotify:function(){}}",
             });
         }
-    }/* else {
-        await download_notify();
-        if (content.indexOf("京东多合一签到") > 0 && content.indexOf("@NobyDa") > 0) {
-            console.log("京东多合一签到通知注入成功");
-            replacements.push({
-                key: "var LogDetails = false;",
-                value: `const lxk0301Notify = require('./sendNotify');var LogDetails = false;`,
-            });
-            replacements.push({
-                key: `if (!$nobyda.isNode) $nobyda.notify("", "", Name + one + two + three + four + disa + notify);`,
-                value: `console.log("通知开始");lxk0301Notify.sendNotify("京东多合一签到", one + two + three + notify);console.log("通知结束");`,
-            });
-        }
-    }*/
-}*/
+    }
+}
 async function downloader(content) {
     if (content.indexOf("jdFruitShareCodes") > 0) {
         await download_jdFruit();
     }
-}
-
-async function download_notify() {
-    await download("https://github.com/lxk0301/scripts/raw/master/sendNotify.js", "./", {
-        filename: "sendNotify.js",
-    });
-    console.log("下载通知代码完毕");
 }
 
 module.exports = {
