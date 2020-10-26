3e81440f2f4dc3a6ef473e4ff117381569cfcff6
qhq <qhqcz@qq.com>
2020-10-26 23:05:02 +0800

    Update jd_price_detect.js

--- Scripts/jd_price_detect.js
+++ Scripts/jd_price_detect.js
@@ -40,7 +40,8 @@ async function deleteFile(path) {
 
 //启动
 async function start() {
-    console.log(`当前执行时间:${new Date().toString()}`);
+console.log(`国际时间 (UTC)：${new Date().toLocaleString()}\n`)
+console.log(`北京时间 (UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}\n`)
     if (!Secrets.DETECT_URL) {
         console.log("请填写 Secrets 后在继续");
         return;
