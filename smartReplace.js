8858f220c6780cb55953670faaf39a9469bcbdd8
qhq <qhqcz@qq.com>
2020-10-26 22:49:18 +0800

    Update smartReplace.js

--- smartReplace.js
+++ smartReplace.js
@@ -4,14 +4,21 @@ const download = require("download");
 async function replaceWithSecrets(content, Secrets) {
     if (!Secrets || !Secrets) return content;
     const replacements = [];
-    await init_notify(Secrets, content);
+    await init_notify(Secrets, content, replacements);
         if (Secrets.DETECT_URL) {
-            //replacements.push({ key: /url = []/, value: "url = " + JSON.stringify(Secrets.DETECT_URL.split("\n")) });
-            replacements.push({ key: "$.detect_url = []", value: `qhq` });
-            //replacements.push({ key: /price = \[\]/, value: "price = " + JSON.stringify(Secrets.DETECT_PRICE.split("\n")) });
+            replacements.push({ key: /url = \[\]/, value: "url = " + JSON.stringify(Secrets.DETECT_URL.split("\n")) });
+            replacements.push({ key: /price = \[\]/, value: "price = " + JSON.stringify(Secrets.DETECT_PRICE.split("\n")) });
         }
         await downloader(content);//检查所需额外js
-
+    /*
+        if (Secrets.MarketCoinToBeanCount && !isNaN(Secrets.MarketCoinToBeanCount)) {
+            let coinToBeanCount = parseInt(Secrets.MarketCoinToBeanCount);
+            if (coinToBeanCount >= 0 && coinToBeanCount <= 20 && content.indexOf("$.getdata('coinToBeans')") > 0) {
+                console.log("蓝币兑换京豆操作已注入");
+                replacements.push({ key: "$.getdata('coinToBeans')", value: coinToBeanCount });
+            }
+        }
+     */
     return batchReplace(content, replacements);
 }
 function batchReplace(content, replacements) {
@@ -21,7 +28,7 @@ function batchReplace(content, replacements) {
     return content;
 }
 
-async function init_notify(Secrets, content) {
+async function init_notify(Secrets, content, replacements) {
     if (!Secrets.PUSH_KEY && !Secrets.BARK_PUSH && !Secrets.TG_BOT_TOKEN) {
         if (content.indexOf("require('./sendNotify')") > 0) {
             replacements.push({
@@ -30,12 +37,32 @@ async function init_notify(Secrets, content) {
                     "{sendNotify:function(){},serverNotify:function(){},BarkNotify:function(){},tgBotNotify:function(){}}",
             });
         }
-    }
+    }/* else {
+        await download_notify();
+        if (content.indexOf("京东多合一签到") > 0 && content.indexOf("@NobyDa") > 0) {
+            console.log("京东多合一签到通知注入成功");
+            replacements.push({
+                key: "var LogDetails = false;",
+                value: `const lxk0301Notify = require('./sendNotify');var LogDetails = false;`,
+            });
+            replacements.push({
+                key: `if (!$nobyda.isNode) $nobyda.notify("", "", Name + one + two + three + four + disa + notify);`,
+                value: `console.log("通知开始");lxk0301Notify.sendNotify("京东多合一签到", one + two + three + notify);console.log("通知结束");`,
+            });
+        }
+    }*/
 }
 async function downloader(content) {
     if (content.indexOf("jdFruitShareCodes") > 0) {
         await download_jdFruit();
     }
+}
+
+async function download_notify() {
+    await download("https://github.com/lxk0301/scripts/raw/master/sendNotify.js", "./", {
+        filename: "sendNotify.js",
+    });
+    console.log("下载通知代码完毕");
 }
 
 module.exports = {
