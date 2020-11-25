c596b419ec72bb6774185668135d618caff3815c
qhq <qhqcz@qq.com>
2020-11-25 11:31:26 +0800

    Update qqyd.js

--- Scripts/qqyd.js
+++ Scripts/qqyd.js
@@ -46,7 +46,7 @@ async function start() {
 //console.log(` 当前执行时间:${new Date().toString()}`);
 console.log(`国际时间 (UTC+00)：${new Date().toLocaleString()}`)
 console.log(`北京时间 (UTC+08)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}\n`)
-    if (!Secrets.JD_COOKIE) {
+    if (!Secrets.COOKIE_QQYD) {
         console.log("请填写 Secrets 后再继续");
         await notify.sendNotify(`${$.name}` + `${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}`, "请填写 Secrets");
         return;
