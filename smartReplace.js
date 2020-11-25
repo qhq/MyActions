28011ddf62a05e9413e2d53e6eacb4891cecb26a
qhq <qhqcz@qq.com>
2020-11-25 22:56:12 +0800

    Update smartReplace.js

--- smartReplace.js
+++ smartReplace.js
@@ -32,8 +32,8 @@ async function replaceWithSecrets(content, Secrets) {
             replacements.push({ key: "$.getdata(qqreadbodyKey)", value: JSON.stringify(Secrets.COOKIE_QQYD.split("\n")[2]) });
             replacements.push({ key: "$.getdata(qqreadtimeurlKey)", value: JSON.stringify(Secrets.COOKIE_QQYD.split("\n")[3]) });
             replacements.push({ key: "$.getdata(qqreadtimeheaderKey)", value: JSON.stringify(Secrets.COOKIE_QQYD.split("\n")[4]) });
-            //replacements.push({ key: "大概5分钟", value: `大概5分钟\nconst task = '';` });
-            replacements.push({ key: "task = JSON.parse(data)", value: `const task = JSON.parse(data)\nconsole.log(task);` });
+            replacements.push({ key: "大概5分钟", value: `大概5分钟\nvar task = '';` });
+            replacements.push({ key: "task = JSON.parse(data)", value: `task = JSON.parse(data)\nconsole.log(task);` });
         }
         await downloader(content);//检查所需额外js
     /*
