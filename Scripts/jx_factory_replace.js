cc58b5e3f87bb6518965e99a9bc6a00b6cc809d5
qhq <qhqcz@qq.com>
2020-12-02 20:50:28 +0800

    Update jx_factory_replace.js

--- Scripts/jx_factory_replace.js
+++ Scripts/jx_factory_replace.js
@@ -27,12 +27,12 @@ async function downFile() {
 
 async function changeFiele(content, cookie) {
     //替换各种信息.
-    content = content.replace("require('./jdCookie.js')", JSON.stringify(`[${cookie} ]`))
+    content = content.replace("require('./jdCookie.js')", `{CookieJD:'${cookie}'}`)
     //content = content.replace(`$.getdata('CookieJD') || '', $.getdata('CookieJD2') || ''`, `'${cookie}',`)
     
     //替换源脚本中推送函数阻止推送
     content = content.replace("require('./sendNotify')", "{sendNotify:function(){},serverNotify:function(){},BarkNotify:function(){},tgBotNotify:function(){},ddBotNotify:function(){},iGotNotify:function(){}}")
-    //console.log(content);
+    console.log(content);
     await fs.writeFileSync('./execute.js', content, 'utf8')
 }
 
