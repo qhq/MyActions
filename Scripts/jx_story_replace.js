c00a1e43bcd6a7f799bc0ef5bb77accc3fce932a
qhq <qhqcz@qq.com>
2020-12-02 22:22:47 +0800

    Update jx_story_replace.js

--- Scripts/jx_story_replace.js
+++ Scripts/jx_story_replace.js
@@ -33,7 +33,7 @@ async function changeFiele(content, cookie) {
     content = content.replace("$.getdata('jx_notifyTime');", "'14:21'")
     
     //替换源脚本中推送函数阻止推送
-    content = content.replace("require('./sendNotify')", "{sendNotify:function(){},serverNotify:function(){},BarkNotify:function(){},tgBotNotify:function(){},ddBotNotify:function(){},iGotNotify:function(){}}")
+    //content = content.replace("require('./sendNotify')", "{sendNotify:function(){},serverNotify:function(){},BarkNotify:function(){},tgBotNotify:function(){},ddBotNotify:function(){},iGotNotify:function(){}}")
     console.log(content);
     await fs.writeFileSync('./execute.js', content, 'utf8')
 }
@@ -55,8 +55,8 @@ async function executeOneByOne() {
         await changeFiele(content, Cookies[i]);
         console.log("替换变量完毕");
         try {
-            //await exec("node execute.js", { stdio: "inherit" });//根据源脚本进行通知
-            await exec("node execute.js >> result.txt")//根据返回内容判断进行通知
+            await exec("node execute.js", { stdio: "inherit" });//根据源脚本进行通知
+            //await exec("node execute.js >> result.txt")//根据返回内容判断进行通知
         } catch (e) {
             console.log("执行异常:" + e);
             await notify.sendNotify(`${d.toLocaleString('chinese',{hour12:false})}`, "执行异常:" + e);
