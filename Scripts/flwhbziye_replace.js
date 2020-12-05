47c0afcbd5c71986adc97e5b0689c029a81fca2d
qhq <qhqcz@qq.com>
2020-12-05 13:21:24 +0800

    Update flwhbziye_replace.js

--- Scripts/flwhbziye_replace.js
+++ Scripts/flwhbziye_replace.js
@@ -28,14 +28,14 @@ async function downFile() {
 async function changeFiele(content, cookie) {
     //替换各种信息.
     content = content.replace("Env(jsname)", `Env(jsname);\nconst notify = $.isNode() ? require('./sendNotify') : '';`)
-    content  = content.replace("$.getdata(flwhburlKey)", `"${cookie.split("\n")[0]}"`)
+    content  = content.replace("$.getdata(flwhburlKey)", "\"https\:\/\/huodong\.fanli\.com\/h5\/Fanlishare20201212\/ajaxInit\"")
     content  = content.replace("$.getdata(flwhbheaderKey)", JSON.stringify(cookie.split("\n")[1]))
     //content  = content.replace("var tz=''", "var tz=''\nall()")
     //content = content.replace("$.msg(jsname,''", "notify.sendNotify(jsname")
     
     //替换源脚本中推送函数阻止推送
     //content = content.replace("require('./sendNotify')", "{sendNotify:function(){},serverNotify:function(){},BarkNotify:function(){},tgBotNotify:function(){},ddBotNotify:function(){},iGotNotify:function(){}}")
-    //console.log(content);
+    console.log(content);
     await fs.writeFileSync('./execute.js', content, 'utf8')
 }
 
