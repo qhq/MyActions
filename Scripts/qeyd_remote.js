85538bd5d9cda91f5811b48adf27933d8b3ba9bb
xinhunshang <50572484+xinhunshang@users.noreply.github.com>
2020-12-11 17:09:04 +0800

    Update qeyd_remote.js

--- Scripts/qeyd_remote.js
+++ Scripts/qeyd_remote.js
@@ -103,8 +103,6 @@ async function msg(content) {
     console.log(content);
     console.log('--------------------');
     var reg =/【现金余额】:([1-9]\d*\.?\d*|0\.\d*[1-9])元/g;
-    console.log(content);
-    console.log(reg.exec(content));
     var gold = parseInt(reg.exec(content)[1].trim());
     if (d.getHours()==8 && d.getMinutes()<=22) {
         await notify.sendNotify(`${d.toLocaleString('chinese',{hour12:false})}`, content);
