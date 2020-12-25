9a6c1d4c041f09318ae40ff82a0e369b63ad587f
qhq <qhqcz@qq.com>
2020-12-25 21:55:52 +0800

    Update qeyd_remote.js

--- Scripts/qeyd_remote.js
+++ Scripts/qeyd_remote.js
@@ -117,8 +117,8 @@ async function msg(content) {
 async function start() {
     //console.log(`当前执行时间:${new Date().toString()}`);
     console.log(`国际时间 (UTC+00)：${new Date().toLocaleString('chinese',{hour12:false})}`)
-    console.log(`北京时间 (UTC+08)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString('chinese',{hour12:false})}\n`)
-    console.log(`脚本引用：` + Secrets.SyncUrl)
+    console.log(`北京时间 (UTC+08)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString('chinese',{hour12:false})}`)
+    console.log(`脚本引用：${Secrets.SyncUrl}\n`)
     if (!Secrets.COOKIE_QEYD) {
         console.log("请填写 COOKIE_QEYD 后在继续");
         return;
