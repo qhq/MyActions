71a747d1c7169951c2baf17b40b4ca414accce8a
qhq <qhqcz@qq.com>
2021-01-05 10:30:13 +0800

    Update qeydbox_remote.js

--- Scripts/qeydbox_remote.js
+++ Scripts/qeydbox_remote.js
@@ -43,8 +43,8 @@ async function changeFiele(content, cookie) {
     content = content.replace("let qqreadtimeHD = [];", `let qqreadtimeHD = [${JSON.stringify(cookie.split("@")[2])}];`)
     content = content.replace(/function showmsg/, `function showmsg() {console.log(kz)}\nfunction GG`)
     
-    content = content.replace(`if (box.code == 0 && box.data.amount)`,"tz +=`${JSON.stringify(box)}\\n`;\nif (box.data === null){\ntz +=`【宝箱已被其他进程打开】\\n`;}\n else")
-    content = content.replace(`if (box.code == 0 && box.data.amount)`,"if (box.data === null){\ntz +=`【宝箱已被其他进程打开】\\n`;}\n else")
+    //content = content.replace(`if (box.code == 0 && box.data.amount)`,"tz +=`${JSON.stringify(box)}\\n`;\nif (box.data === null){\ntz +=`【宝箱已被其他进程打开】\\n`;}\n else")
+    content = content.replace(`if (box.code == 0 && box.data.amount)`,"if (box.data === null){\nkz +=`【宝箱已被其他进程打开】\\n`;}\n else")
 
     //替换源脚本中推送函数阻止推送
     //content = content.replace("require('./sendNotify')", "{sendNotify:function(){},serverNotify:function(){},BarkNotify:function(){},tgBotNotify:function(){},ddBotNotify:function(){},iGotNotify:function(){}}")
