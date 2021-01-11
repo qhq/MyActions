c4852b14e43a17afcbd905255bf1203f2b03441d
qhq <qhqcz@qq.com>
2021-01-11 12:46:18 +0800

    Update jd_shareCode_auto.js

--- Scripts/jd_shareCode_auto.js
+++ Scripts/jd_shareCode_auto.js
@@ -1,5 +1,5 @@
 const $ = new Env("京东分享码");
-const notify = $.isNode() ? require("./sendNotify") : "";
+const notify = $.isNode() ? require("../sendNotify") : "";
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
 const JD_API_HOST = "https://api.m.jd.com/client.action";
